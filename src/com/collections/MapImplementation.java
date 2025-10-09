4ac20486f30589b380f201a72033cfd64ed8e3d4
TILAK1818 <tilak.muralidharan@gmail.com>
2025-10-09 18:31:40 +0530

    Day 8 Collections completed

---
+++ src/com/collections/MapImplementation.java
@@ -0,0 +1,15 @@
+package com.collections;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapImplementation {
+	public static void main(String[] args) {
+		Map<Integer,String> map1=new HashMap<>();
+		map1.put(1,"Tilak");
+		map1.put(2, "kumar");
+		System.out.println(map1);
+		
+	}
+	
+}
